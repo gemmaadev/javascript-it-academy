7ff7fb472c289227c5b6a9c9361ec45012d48a67
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-02 17:15:08 +0200

    feat: solve 06_modules exercises - imports, exports and node_modules

--- common/Mathy.js
+++ common/Mathy.js
@@ -1,13 +1,17 @@
-export {sqrt, square, diag}
+export { sqrt, square, diag, add };
 
 function sqrt(num) {
-  return Math.sqrt(num)
+  return Math.sqrt(num);
 }
 
 function square(num) {
-  return num * num
+  return num * num;
 }
 
 function diag(x, y) {
-  return sqrt(square(x) + square(y))
+  return sqrt(square(x) + square(y));
+}
+
+function add(a, b) { //afegida perquè no existia
+  return a + b;
 }
--- exercises/06_modules.test.js
+++ exercises/06_modules.test.js
@@ -1,48 +1,53 @@
-import * as Mathy from '../common/Mathy'
-import * as IndexImport from '../common'
+import * as Mathy from "../common/Mathy";
+import * as IndexImport from "../common";
+import { sqrt as mySqrt, square as mySquare } from "../common/Mathy";
+import _ from "lodash";
+import { add } from "../common/Mathy";
+import lodash from "lodash";
+
 // WRITE YOUR IMPORT STATEMENTS HERE
 
-test('can import Mathy', () => {
+test("can import Mathy", () => {
   // this one's already done! You're welcome :)
-  expect(Mathy.sqrt).toBeDefined()
-  expect(Mathy.square).toBeDefined()
-  expect(Mathy.diag).toBeDefined()
-})
+  expect(Mathy.sqrt).toBeDefined();
+  expect(Mathy.square).toBeDefined();
+  expect(Mathy.diag).toBeDefined();
+});
 
-test('06_modules-1: can specify what to import, to only retain pieces of the import', () => {
+test("06_modules-1: can specify what to import, to only retain pieces of the import", () => {
   // Import `Mathy` again, but pull out only the `sqrt` as mySqrt, and `square` as mySquare
-  expect(mySqrt).toBeDefined()
-  expect(mySquare).toBeDefined()
-  expect(mySqrt).toBe(Mathy.sqrt)
-  expect(mySquare).toBe(Mathy.square)
-})
+  expect(mySqrt).toBeDefined();
+  expect(mySquare).toBeDefined();
+  expect(mySqrt).toBe(Mathy.sqrt);
+  expect(mySquare).toBe(Mathy.square);
+});
 
-test('06_modules-1: can import from my node_modules', () => {
+test("06_modules-1: can import from my node_modules", () => {
   // import `lodash`
-  expect(_).toBeDefined()
-})
+  expect(_).toBeDefined();
+});
 
-test('06_modules-2: can import Mathy', () => {
+test("06_modules-2: can import Mathy", () => {
   // Importa el mòdul Mathy i comprova que funciona correctament
-  expect(Mathy.add(1, 2)).toBe(3)
-})
+  expect(Mathy.add(1, 2)).toBe(3);
+});
 
-test('06_modules-3: can specify what to import, to only retain pieces of the import', () => {
+test("06_modules-3: can specify what to import, to only retain pieces of the import", () => {
   // Importa només les parts necessàries del mòdul Mathy
-  expect(add(1, 2)).toBe(3)
-})
+  expect(add(1, 2)).toBe(3);
+});
 
-test('06_modules-4: can import from my node_modules', () => {
+test("06_modules-4: can import from my node_modules", () => {
   // Importa un mòdul des de node_modules i comprova que funciona
-  expect(lodash.isEmpty([])).toBe(true)
-})
+  expect(lodash.isEmpty([])).toBe(true);
+});
 
 //////// EXTRA CREDIT ////////
-test.skip('Index import', () => {
+test.skip("Index import", () => {
   //I have noticed that using index.js is pretty common pattern
   //If someone has been confused about that maybe this helps
-  expect(IndexImport.variable1).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable2).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable3).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable4).toBe(/* ENTER YOUR GUESS HERE */)
-})
+  expect(IndexImport.variable1).toBe("Bob");
+  expect(IndexImport.variable2).toBe("Kent");
+  expect(IndexImport.variable3).toBe(222);
+  expect(IndexImport.variable4).toBe(false);
+});
