6e523a2b36223d4d3d03cf25efe994662b52e155
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-03 11:34:12 +0200

    feat: solve 09_spread exercises - spread operator with functions, arrays and objects

--- exercises/09_spread.test.js
+++ exercises/09_spread.test.js
@@ -1,38 +1,45 @@
-test('09_spread-1: should be able to call a function and spread the arguments', () => {
+test("09_spread-1: should be able to call a function and spread the arguments", () => {
   // Utilitza l'operador spread per passar arguments a una funció
-  const args = ['a', 'b', 'c']
-  let calls = 0
+  const args = ["a", "b", "c"];
+  let calls = 0;
 
   const myFunction = (a, b, c) => {
-    expect(a).toBe('a')
-    expect(b).toBe('b')
-    expect(c).toBe('c')
-    calls++
-  }
-  
-  expect(calls).toBe(1)
+    expect(a).toBe("a");
+    expect(b).toBe("b");
+    expect(c).toBe("c");
+    calls++;
+  };
 
-})
+  myFunction(...args);
 
-test('09_spread-2: should be easier to concatenate arrays', () => {
+  expect(calls).toBe(1);
+});
+
+test("09_spread-2: should be easier to concatenate arrays", () => {
   // Utilitza l'operador spread per concatenar arrays
-  const array1 = [1, 2, 3]
-  
-  
-  expect(result).toEqual([1, 2, 3, 4, 5, 6])
-})
+  const array1 = [1, 2, 3];
+  const array2 = [4, 5, 6];
+  const result = [...array1, ...array2];
 
-test('09_spread-3: should be able to merge properties from objects', () => {
-   // crea un objecte resultat que utilitzi l'operador spread per afegir `eggs: 'spam'` al que existeix a obj1
+  expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test("09_spread-3: should be able to merge properties from objects", () => {
+  // crea un objecte resultat que utilitzi l'operador spread per afegir `eggs: 'spam'` al que existeix a obj1
   const obj1 = {
-    foo: 'bar',
-    baz: 'foobar',
-  }
+    foo: "bar",
+    baz: "foobar",
+  };
 
+  const obj2 = {
+    eggs: "spam",
+  };
+
+  const result = { ...obj1, ...obj2 };
 
   expect(result).toEqual({
-    foo: 'bar',
-    baz: 'foobar',
-    eggs: 'spam',
-  })
-})
+    foo: "bar",
+    baz: "foobar",
+    eggs: "spam",
+  });
+});
