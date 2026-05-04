1e1c14b220dd2949dd5802f7cb0b9fce4c01dbdf
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-04 12:11:21 +0200

    feat: solve 15_async-await exercises

--- exercises/15_async-await.test.js
+++ exercises/15_async-await.test.js
@@ -1,30 +1,43 @@
 // Reescriu tots aquests tests de promeses a async/await
 
-const doAsync = (rejectPromise = false) =>  new Promise((resolve, reject) => setTimeout(() => {
+const doAsync = (rejectPromise = false) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
       if (rejectPromise) {
-        reject('rejected')
+        reject("rejected");
       } else {
-        resolve('resolved')
+        resolve("resolved");
       }
-    })
-  )
+    }),
+  );
 
-  test('15_async-await-1: should work with resolved promises', async () => {
-  return doAsync().then(result => {
-    expect(result).toBe('resolved')
-    throw new Error(
-      'convert this to an async/await function and remove this error',
-    )
-  })
-})
+test("15_async-await-1: should work with resolved promises", async () => {
+  const result = await doAsync();
+  expect(result).toBe("resolved");
+});
 
-test('15_async-await-2: should throw an error with a rejected promise', async () => {
-  return doAsync(true).catch(error => {
-    expect(error).toBe('rejected')
-    throw new Error(
-      'convert this to an async/await function and remove this error',
-    )
-  })
-})
+// test("15_async-await-1: should work with resolved promises", async () => {
+//   return doAsync().then((result) => {
+//     expect(result).toBe("resolved");
+//     throw new Error(
+//       "convert this to an async/await function and remove this error",
+//     );
+//   });
+// });
 
+test("15_async-await-2: should throw an error with a rejected promise", async () => {
+  try {
+    await doAsync(true);
+  } catch (error) {
+    expect(error).toBe("rejected");
+  }
+});
 
+// test("15_async-await-2: should throw an error with a rejected promise", async () => {
+//   return doAsync(true).catch((error) => {
+//     expect(error).toBe("rejected");
+//     throw new Error(
+//       "convert this to an async/await function and remove this error",
+//     );
+//   });
+// });
