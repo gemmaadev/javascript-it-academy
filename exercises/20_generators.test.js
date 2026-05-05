c228541b04e3fdb51d7e16db9ed7c46f93b4d061
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-05 11:06:14 +0200

    feat: solve 20_generators exercises - generator functions, yield and iteration

--- exercises/20_generators.test.js
+++ exercises/20_generators.test.js
@@ -1,38 +1,38 @@
-test('20_generators-1: should yield objects with value and done properties', () => {
-  const odds = giveMeOneOddNumber()
+test("20_generators-1: should yield objects with value and done properties", () => {
+  const odds = giveMeOneOddNumber();
 
   // Comprova que els valors generats tenen les propietats value i done
-  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-  expect(odds.next().done).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-  odds.next()
-  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-  expect(odds.next().done).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
+  expect(odds.next().value).toBe(1);
+  expect(odds.next().value).toBe(3);
+  expect(odds.next().done).toBe(false);
+  odds.next();
+  expect(odds.next().value).toBe(9);
+  expect(odds.next().done).toBe(true);
 
   function* giveMeOneOddNumber() {
-    yield 1
-    yield 3
-    yield 5
-    yield 7
-    yield 9
+    yield 1;
+    yield 3;
+    yield 5;
+    yield 7;
+    yield 9;
   }
-})
+});
 
-test('20_generators-2: can be iterated over', () => {
+test("20_generators-2: can be iterated over", () => {
   function* giveMeOneEvenNumber() {
-    yield 2
-    yield 4
-    yield 6
-    yield 8
+    yield 2;
+    yield 4;
+    yield 6;
+    yield 8;
   }
 
-  let sum = 0
+  let sum = 0;
 
   // ATENCIÓ, AIXÒ ÉS BLOQUEJANT/SINCRÒNIC!
   // Els generadors no són async/await, aquests poden estar a ES2016
   for (let even of giveMeOneEvenNumber()) {
-    sum = sum + even
+    sum = sum + even;
   }
 
-  expect(sum).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-})
+  expect(sum).toBe(20);
+});
