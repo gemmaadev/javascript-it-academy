05a4643caed5361f3254823f59f1516169a4c6ce
Gemma Maeso <gemmaa.dev@gmail.com>
2026-05-05 13:10:35 +0200

    feat: solve 25_es2023 exercises

--- exercises/25_es2023.test.js
+++ exercises/25_es2023.test.js
@@ -1,24 +1,24 @@
-test('25_es2023-1: Array findLast and findLastIndex', () => {
+test("25_es2023-1: Array findLast and findLastIndex", () => {
   //Utilitza els mètodes findLast i findLastIndex per trobar l'últim número parell i el seu índex.
-  const array = [1, 2, 3, 4, 5]
+  const array = [1, 2, 3, 4, 5];
 
-  const lastEven = null // Resol aquí...
-  const lastEvenIndex = null // Resol aquí...
+  const lastEven = array.findLast((number) => number % 2 === 0);
+  const lastEvenIndex = array.findLastIndex((number) => number % 2 === 0);
 
-  expect(lastEven).toBe(4)
-  expect(lastEvenIndex).toBe(3)
-})
+  expect(lastEven).toBe(4);
+  expect(lastEvenIndex).toBe(3);
+});
 
-test('25_es2023-2: Hashbangs in JavaScript files', () => {
+test("25_es2023-2: Hashbangs in JavaScript files", () => {
   //Escriu un script amb un hashbang i comprova que no causa errors.
-  const script = null // Resol aquí...
+  const script = "#!/usr/bin/env node\n console.log('Hola!');";
 
-  expect(() => eval(script)).not.toThrow()
-})
+  expect(() => eval(script)).not.toThrow();
+});
 
-test('25_es2023-3: Symbol.prototype.description', () => {
+test("25_es2023-3: Symbol.prototype.description", () => {
   //Crea un símbol amb una descripció i comprova que la descripció és correcta.
-  const symbol = null // Resol aquí...
+  const symbol = Symbol("description");
 
-  expect(symbol.description).toBe('description')
-})
+  expect(symbol.description).toBe("description");
+});
